0ed86774271a6617c79d336b6cf5f4b504305799
ErikaGalanCont <102776229+ErikaGalanCont@users.noreply.github.com>
2023-02-15 21:02:01 -0500

    For

---
+++ main.js
@@ -0,0 +1,38 @@
+// Equivalencias
+// true                false
+//  -infinito 1          0
+//  " "                 ""
+//  [] - {}     null - undefined
+//
+// num = Number(prompt("Ingrese su edad"))
+// switch (true) {
+//     case num>=18 && num<=20:
+//         console.log("Es mayor de edad");
+//         break;
+//     case num>=100 && num<=150:
+//         console.log("xxxxx");
+//         break;
+//     default:
+//         console.log("No cumple la condicion");
+//         break;
+// 
+// let i = 0;
+// let bandera = true;
+// for(i; bandera;){
+//     console.log(bandera);
+//     bandera = (i<5) ? ++i: false;
+// }
+
+
+
+let num= Number(prompt("ingrese un numero"));
+
+for (let j = 1; j <= num; j++) {
+    for (let i = 1; i<=10; i++) {
+        console.log(`${j} X ${i} =${j*i}`);  
+    }
+}
+
+
+
+
